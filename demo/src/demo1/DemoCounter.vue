873a12ac906dcdcd2604ce5d9400191f0497faaf
Nataliya Nikulshina <victory515@yandex.ru>
2024-05-24 23:34:25 +0300

    рефакторинг и бэклог

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="counter">
+        <button @click="decrement"> - </button>
+
+        <span> Current value: {{ count }} </span>
+
+        <button @click="increment"> + </button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+import DemoContainer from '../components/DemoContainer.vue';
+
+const count = ref(0)
+
+/* Метод уменьшения значения */
+const decrement = () => {
+  count.value = count.value - 1
+}
+
+/* Метод увеличения значения */
+const increment = () => {
+  count.value = count.value + 1
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
